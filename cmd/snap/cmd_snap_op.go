64d29e8a5ca47a6df568ffa7efc06223b1327337
John R. Lenton <jlenton@gmail.com>
2019-08-28 16:35:25 +0100

    cmd/snap: use Name not Full() in message; improve tests
    In the deprecation message we were using Full() instead Name, which
    meant "/stable" got printed as "latest/stable" even though what we
    want (and use) is actually "stable". This fixes that.
    
    Also improved test by checking that the client sends what we want, and
    that ///foo/stable// is also covered just in case.
    
    Thanks to @pedronis, @mvo5 and @anonymouse64 for spotting these.

--- cmd/snap/cmd_snap_op.go
+++ cmd/snap/cmd_snap_op.go
@@ -260,7 +260,7 @@ func (mx *channelMixin) setChannelFromCommandline() error {
 			}
 			// TODO: get escapes in here so we can bold the Warning
 			head := i18n.G("Warning:")
-			warn := fill(fmt.Sprintf(i18n.G("Specifying a channel %q is relying on undefined behaviour. Interpreting it as %q for now, but this will change.\n"), mx.Channel, ch.Full()), utf8.RuneCountInString(head)+1) // +1 for the space
+			warn := fill(fmt.Sprintf(i18n.G("Specifying a channel %q is relying on undefined behaviour. Interpreting it as %q for now, but this will change.\n"), mx.Channel, ch.Name), utf8.RuneCountInString(head)+1) // +1 for the space
 			fmt.Fprint(Stderr, head, " ", warn, "\n\n")
 		}
 		// because snap.ParseChannel calls Clean() on the channel,
